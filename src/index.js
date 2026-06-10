6e01160bfbbb385979bebe4ec2cf0e4f895f1d95
Purple-Watermelon <lemons0058@gmail.com>
2026-06-10 10:45:36 +0900

    fix: 서비스워커 경로를 앱 주소 기준으로 수정 (index.js)

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ root.render(<React.StrictMode><App /></React.StrictMode>);
 // PWA Service Worker 등록
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register(process.env.PUBLIC_URL + '/service-worker.js')
       .then(reg => console.log('SW registered:', reg.scope))
       .catch(err => console.log('SW registration failed:', err));
   });
